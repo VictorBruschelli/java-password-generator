1ad1d66a6197e915af5a5b96c4d2974ba351cf0f
victor bruschelli <165551238+VictorBruschelli@users.noreply.github.com>
2024-06-15 18:21:31 +0200

    add valoreUtente.close();

--- src/main/java/org/lessons/java/security/PasswordGenerator.java
+++ src/main/java/org/lessons/java/security/PasswordGenerator.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		
-		/*salvare in opportune variabili il <nome>, <cognome>, <colore preferito> e <data di nascita> di un utente
-		  suddivisa in giorno, mese e anno in numero
-          generare (e stampare a video) una password concatenando
-          nome, cognome, colore preferito e somma di giorno, mese e anno di nascita, 
-          separate dal carattere -
-          Esempio: ho un utente che si chiama Pinco Pallo, nato il 12/05/1994,
-           il cui colore preferito è il magenta La sua password sarà Pinco-Pallo-magenta-2011*/
-		
+	
+	/*salvare in opportune variabili il <nome>, <cognome>, <colore preferito> e <data di nascita> di un utente
+	  suddivisa in giorno, mese e anno in numero
+      generare (e stampare a video) una password concatenando
+      nome, cognome, colore preferito e somma di giorno, mese e anno di nascita, 
+      separate dal carattere -
+      Esempio: ho un utente che si chiama Pinco Pallo, nato il 12/05/1994,
+       il cui colore preferito è il magenta La sua password sarà Pinco-Pallo-magenta-2011*/
+	
 
 		Scanner valoreUtente=new Scanner(System.in);
 		System.out.println("password generation");
@@ -34,11 +34,14 @@ public class PasswordGenerator {
 		//anno
 		System.out.println("inserisci l'anno di nascita:");
 		int anno = valoreUtente.nextInt();
+		valoreUtente.close();
 		
 		int sommaData = giorno + mese + anno;
 		
 		//password
 		System.out.println("la tua password è: "+nomeUtente+"-"+cognomeUtente+"-"+coloreUtente+"-"+sommaData);
+		
+		
 	}
 
 }
